c42c57aeceae40d856519a980a6e9e7471351adc
LordYoungblood <63210180+LordYoungblood@users.noreply.github.com>
2022-08-10 11:48:49 -0400

    added CSS

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # SDI Blended Project 1 Scaffold
 
-Add any files you see fit for your application to work. Make sure to change this README to include a description of your application and how to get it started.
+Add any files you see fit for your application to work. Make sure to change this README to include a description of your application and how to get it started. You can also add any other files you want to help you get started.
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-//// this is some random text for testing
+let shufflebtn = document.getElementById('btn1')
+let drawbtn = document.getElementById('btn2')
+let cardimage = document.getElementById('image1')
+let carddict = {};
+
+shufflebtn.addEventListener('click', function() {
+    fetch("http://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,2S,KS,AD,2D,KD,AC,2C,KC,AH,2H,KH")
+    .then(response => response.json())
+    .then(data => console.log(data.deck_id))
+})
