21d08fc8da978eb1f45d948e6b4542bf12cfbb22
IGardocki <ian.d.gardocki@gmail.com>
2022-08-10 12:07:30 -0500

    finished JS fetch for shuffle/draw

--- scripts/main.js
+++ scripts/main.js
@@ -8,5 +8,19 @@ let carddict = {};
 shufflebtn.addEventListener('click', function() {
     fetch("http://deckofcardsapi.com/api/deck/new/shuffle/?cards=AS,2S,KS,AD,2D,KD,AC,2C,KC,AH,2H,KH")
     .then(response => response.json())
-    .then(data => console.log(data.deck_id))
+    .then(data => data.deck_id)
+    .then(deck_ID => {
+        drawbtn.addEventListener('click', function(){
+            fetch(`http://deckofcardsapi.com/api/deck/${deck_ID}/draw/?count=1`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.cards[0].code)
+                console.log(data.cards[0].image)
+                let imagelink = data.cards[0].images.svg
+                cardimage.src = imagelink
+                // " image1" src = ""
+            });
+        })
+    })
 })
+
